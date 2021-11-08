ad00bd0d14c86d61e451fddf6a6c7dd80d6316d0
jeonse3875 <devbyseke@gmail.com>
2021-11-08 13:27:36 +0900

    Implement rough SPH logic

---
+++ Source/SPH2003/SPH2003.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SPH2003.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SPH2003, "SPH2003" );
---
+++ Source/SPH2003/SPH2003GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SPH2003GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPH2003_API ASPH2003GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
